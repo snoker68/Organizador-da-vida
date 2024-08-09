2059d11db480e2c8a6b3359e4e4e94bc524ab8f9
snoker68 <danilosoaressantos4@gmail.com>
2024-08-09 10:33:13 -0300

    Modificação no codigo, logica criada para importação no google calender

--- Main.py
+++ Main.py
@@ -1,19 +1,37 @@
-import argparse
-from datetime import datetime
+from icalendar import Calendar, Event,Alarm
+from datetime import  datetime,timedelta
+
+titulo = input("Digite nome do evento:")
+descricao = input("Descreva seu evento")
+
+data_inicio_str = input("Digite data e a hora do inicio do evento:")
+data_inicio = datetime.strftime(data_inicio_str, "%d/%m/%y %H:%M")
+
+data_termino_str = input("Digite data e a hora do termino do evento:")
+data_termino = datetime.strftime(data_termino_str, "%d/%m/%y %H:%M")
+
+Notificação = int(input("informe quanto minutos voce gostaria de sempre lembrado antes do evento:"))_
 
 
-input ("Escreva sua prioridade!!!")
-print ("Certo seu voce tem uma obrigação agendada")
+calendario = Calendar()
 
-def verficar_Data (data_str): 
-    try:
-        data = datetime.strptime(data_str, "%d/%m/%Y")
-        print(f"A data {data_str} foi resevada no seu calendario")
-        return data
-    except ValueError:
-        print(f"data incorreta {data_str} ela esta incorreta")
-        return None
+evento =Event()
+evento.add('summary', titulo)
+evento.add('description', descricao)
+evento.add('dtstart', data_inicio)
+evento.add('dtend', data_termino)
 
+lembrete = Alarm()
+lembrete.add('action', 'Display')
+lembrete.add('description', "lembrete: " + titulo)
+lembrete.add('trigger', timedelta(minutes=-Notificação))
 
-data =  input("digita data do seu compromisso e a hora (precisa ser nesse formato DD/MM/AAAA)")
-verficar_Data (data),
+evento.add_component(evento)
+
+calendario.add_component(evento)
+
+nome_arquivo = input("digite nome do arquivo para salvar") + ".ics"
+with open(nome_arquivo,'wb') as f:
+    f.write(calendario.to_ical())
+
+print(f"Seu evento foi agendado com nome {nome_arquivo}.a arquivo gerado com sucesso")
